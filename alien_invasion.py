d29016a9c095cde2dbfaa5389efbac88aaaed539
zhangwenqiang <13339945044@163.com>
2024-08-22 20:00:25 +0800

    update alien row_number && alien moving

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -28,6 +29,10 @@ class AlienInvasion:
 
         self.bullets = pygame.sprite.Group()
 
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
@@ -40,7 +45,20 @@ class AlienInvasion:
             self._check_events()
 
             self.ship.update()
-            self.bullets.update()
+
+            # 关于子弹位置的变化 移到 _update_bullet（）方法中了
+            # self.bullets.update()
+            # # 删除消失的子弹
+            # for bullet in self.bullets.copy():
+            #     if bullet.rect.bottom < 0:
+            #         self.bullets.remove(bullet)
+            # print(len(self.bullets))
+
+
+            self._update_bullet()
+
+            self._update_aliens()
+
             # 每次循环时都重绘屏幕。
             self._update_screen()
 
@@ -48,6 +66,44 @@ class AlienInvasion:
             # self.ship.blitme()
             # # 让最近绘制的屏幕可见
             # pygame.display.flip()
+
+
+    def _create_fleet(self):
+        """创建外星人群"""
+        # 创建一个外星人并计算一行可容纳多少个外星人。
+        #  外星人的间距为外星人宽度。
+        alien = Alien(self)
+        alien_width,alien_height = alien.rect.size
+
+        # 计算可以容纳多少行外星人
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+
+        number_alien_x = available_space_x // (2 * alien_width)
+
+        # 计算屏幕可容纳多少行外星人
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+
+        number_rows = available_space_y //(2 * alien_height)
+
+
+
+
+        # 创建第一行外星人
+        for row_number in range(number_rows):
+            for alien_number in range(number_alien_x):
+                self._create_alien(alien_number,row_number)
+
+
+    def _create_alien(self,alien_number,row_number):
+        # 创建一个外星人并将其加入当前行
+        alien = Alien(self)
+        alien_width,alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien_height * row_number
+
+        self.aliens.add(alien)
 
     def _check_events(self):
         """响应鼠标和按键事件"""
@@ -71,7 +127,7 @@ class AlienInvasion:
         #     self.ship.moving_up = True
         # elif event.key == pygame.K_DOWN:
         #     self.ship.moving_down = True
-        elif event.key == pygame.K_SPACE:
+        elif event.key == pygame.K_SPACE :
             self._fire_buttle()
 
 
@@ -89,10 +145,26 @@ class AlienInvasion:
 
     def _fire_buttle(self):
         """创建一颗子弹，并加入其编组buttles"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+
+        if  len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
+    def _update_bullet(self):
+        """更新子弹的位置 并删除消失的子弹"""
+
+        # 更新子弹的位置
+        self.bullets.update()
+
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom < 0:
+                self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """更新外星人群中所有外星人的位置"""
+        self.aliens.update()
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕。"""
@@ -101,6 +173,10 @@ class AlienInvasion:
 
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+
+        self.aliens.draw(self.screen)
+
+
         pygame.display.flip()
 
 
--- settings.py
+++ settings.py
@@ -18,3 +18,13 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
+        self.bullet_allowed = 3
+
+
+        # 外星人设置
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction 表示 外星人群移动的方向  1 表示向右 -1表示向左
+        self.fleet_direction = 1
+
+
